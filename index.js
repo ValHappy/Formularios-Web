2159038de7b477021d02aadb2c1077c23a44e6d2
Valentina Alegría <val.happy.m@gmail.com>
2019-04-20 11:54:57 -0500

    modificacion de archivo arts

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 
 var app = express();
-var fs = require('fs');
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -14,28 +12,8 @@ app.get('/', function(request, response){
     var contexto = {
         titulo: 'Página principal',
     };
-    response.render('home', contexto);
+    response.render('index', contexto);
 });
-
-app.post('/login', function(request, response){
-    var infoFormulario = request.body;
-    agregarInformacion("Correo: "+ infoFormulario.correo + " Contraseña: " + infoFormulario.contrasena + "\n");
-    response.redirect('/welcome');
-});
-
-app.get('/welcome', function(request, response){
-    var contexto = {
-        titulo: 'Bienvenido',
-    };
-    response.render('welcome', contexto);
-});
-
-function agregarInformacion(texto) {
-    fs.appendFile('archivo.txt', texto, function (err) {
-        if (err) throw err;
-        console.log('Información agregada!');
-    });
-}
 
 console.log("Servidor iniciado...");
 app.listen(3000);
